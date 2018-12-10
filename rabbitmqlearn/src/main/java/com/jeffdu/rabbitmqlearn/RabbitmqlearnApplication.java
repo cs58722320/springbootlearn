249d0fb93502d6b9186f34a21b733c37a8dcfd02
JeffDu <904293955@qq.com>
2018-12-10 22:02:29 +0800

    RabbitMQ INIT

--- rabbitmqlearn/src/main/java/com/jeffdu/rabbitmqlearn/RabbitmqlearnApplication.java
+++ rabbitmqlearn/src/main/java/com/jeffdu/rabbitmqlearn/RabbitmqlearnApplication.java
@@ -1,12 +1,12 @@
-package com.jeffdu.springbootlearn;
+package com.jeffdu.rabbitmqlearn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootlearnApplication {
+public class RabbitmqlearnApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootlearnApplication.class, args);
+		SpringApplication.run(RabbitmqlearnApplication.class, args);
 	}
 }
--- rabbitmqlearn/src/test/java/com/jeffdu/rabbitmqlearn/RabbitmqlearnApplicationTests.java
+++ rabbitmqlearn/src/test/java/com/jeffdu/rabbitmqlearn/RabbitmqlearnApplicationTests.java
@@ -1,4 +1,4 @@
-package com.jeffdu.springbootlearn;
+package com.jeffdu.rabbitmqlearn;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringbootlearnApplicationTests {
+public class RabbitmqlearnApplicationTests {
 
 	@Test
 	public void contextLoads() {
