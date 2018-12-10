ea258a63c8db914503ad40707c5f6f2a7f02e3c7
JeffDu <904293955@qq.com>
2018-12-10 22:35:33 +0800

    RabbitMQ SimpleSender ADD

--- rabbitmqlearn/src/test/java/com/jeffdu/rabbitmqlearn/RabbitmqlearnApplicationTests.java
+++ rabbitmqlearn/src/test/java/com/jeffdu/rabbitmqlearn/RabbitmqlearnApplicationTests.java
@@ -1,7 +1,9 @@
 package com.jeffdu.rabbitmqlearn;
 
+import com.jeffdu.rabbitmqlearn.direct.SimpleSender;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class RabbitmqlearnApplicationTests {
 
+	@Autowired
+	private SimpleSender sender;
+
 	@Test
-	public void contextLoads() {
+	public void simpleSenderTest() {
+		sender.send();
 	}
 
 }
