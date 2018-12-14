a20b9f68aa7552f808cea1eff8e3774f3b905844
JeffDu <904293955@qq.com>
2018-12-14 12:45:59 +0800

    RabbitMQ SimpleSender ADD

---
+++ activitylearn/src/main/java/com/jeffdu/activitylearn/ActivitylearnApplication.java
@@ -0,0 +1,14 @@
+package com.jeffdu.activitylearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitylearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitylearnApplication.class, args);
+	}
+
+}
+
