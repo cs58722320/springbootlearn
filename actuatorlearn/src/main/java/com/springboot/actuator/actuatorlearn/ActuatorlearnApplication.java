f0d353349c49fae2fc5bdbe5df369f3e4597c7bc
JeffDu <904293955@qq.com>
2019-02-24 18:47:29 +0800

    Actuator INIT

---
+++ actuatorlearn/src/main/java/com/springboot/actuator/actuatorlearn/ActuatorlearnApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.actuator.actuatorlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorlearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorlearnApplication.class, args);
+	}
+
+}
---
+++ actuatorlearn/src/test/java/com/springboot/actuator/actuatorlearn/ActuatorlearnApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springboot.actuator.actuatorlearn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ActuatorlearnApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
